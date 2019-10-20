26d6c96d289f81ac6bef87af62e03dcca0e76866
devin-bielejec <devin.bieleje@gmail.com>
2019-10-20 17:53:27 -0400

    Knex Migrations

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # back-end
+
+Users
+-id
+-isParent: false
+-firstName
+-lastName
+-age
+-gender
+-description
+
+Requests
+-id
+-userID references id in users (ParentID)
+-accepted: false
+-meetingPlace: string
+-time: time
+-date: date
+-kids: integer
+-description: string
+
+Questions
+-id
+-requestID references id in requests
+-userID (Questioner's ID) references id in users
+-content: string
+-timeStamp: time
+
+Parents
+Navbar: My Requests (Add button)
+: Jobs
+: My Accepted Requests
+: My Accepted Jobs
+
+Nonparent
+: Jobs
+: My accepted jobs
---
+++ migrations/20191020174014_users.js
@@ -0,0 +1,56 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("users", tbl => {
+      tbl.increments();
+      tbl
+        .string("username")
+        .notNullable()
+        .unique();
+      tbl.string("password").notNullable();
+      tbl.string("firstName").notNullable();
+      tbl.string("lastName").notNullable();
+      tbl
+        .bool("isParent")
+        .defaultTo(false)
+        .notNullable();
+    })
+    .createTable("requests", tbl => {
+      tbl.increments();
+      tbl
+        .integer("userID")
+        .notNullable()
+        .references("id")
+        .inTable("users");
+      tbl
+        .bool("accepted")
+        .defaultTo(false)
+        .notNullable();
+      tbl.string("meetingPlace").notNullable();
+      tbl.time("time").notNullable();
+      tbl.date("date").notNullable();
+      tbl.integer("kids").notNullable();
+      tbl.string("description");
+    })
+    .createTable("questions", tbl => {
+      tbl.increments();
+      tbl
+        .integer("requestID")
+        .references("id")
+        .inTable("requests")
+        .notNullable();
+      tbl
+        .integer("userID")
+        .references("id")
+        .inTable("users")
+        .notNullable();
+      tbl.string("content").notNullable();
+      tbl.timestamps(true, true);
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("questions")
+    .dropTableIfExists("requests")
+    .dropTableIfExists("users");
+};
