23903d9f07f2f4d2f1b73afb1bc284423e3a1d34
devin-bielejec <devin.bieleje@gmail.com>
2019-10-20 17:10:06 -0400

    Boilerplate stuff, installations etc

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+//requests
+//questions
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "Sanity Check" });
+});
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+
+module.exports = server;
