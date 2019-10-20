e4fc37a72c86e2847f58048e1de26a529643a288
devin-bielejec <devin.bieleje@gmail.com>
2019-10-20 18:30:24 -0400

    Completed seeds and migrations final

--- data/seeds/002-requests.js
+++ data/seeds/002-requests.js
@@ -8,7 +8,7 @@ exports.seed = function(knex) {
         {
           id: 1,
           userID: 1,
-          accepted: false,
+          accepted: true,
           meetingPlace: "Food Court",
           dateTime: "2019-01-01 20:00:00",
           kids: 4,
@@ -16,7 +16,16 @@ exports.seed = function(knex) {
         },
         {
           id: 2,
-          userID: 5,
+          userID: 1,
+          accepted: false,
+          meetingPlace: "Food Court",
+          dateTime: "2019-01-01 20:00:00",
+          kids: 4,
+          description: "My kids are great!"
+        },
+        {
+          id: 3,
+          userID: 2,
           accepted: true,
           meetingPlace: "Log Flume",
           dateTime: "2019-01-01 11:00:00",
@@ -24,17 +33,35 @@ exports.seed = function(knex) {
           description: "My kids are great!"
         },
         {
-          id: 3,
-          userID: 4,
+          id: 4,
+          userID: 2,
           accepted: false,
+          meetingPlace: "Food Court",
+          dateTime: "2019-01-01 20:00:00",
+          kids: 4,
+          description: "My kids are great!"
+        },
+        {
+          id: 5,
+          userID: 3,
+          accepted: true,
           meetingPlace: "Food Court",
           dateTime: "2019-01-01 15:00:00",
           kids: 1,
           description: "They are great to with!"
         },
         {
-          id: 4,
+          id: 6,
           userID: 3,
+          accepted: false,
+          meetingPlace: "Food Court",
+          dateTime: "2019-01-01 20:00:00",
+          kids: 4,
+          description: "My kids are great!"
+        },
+        {
+          id: 7,
+          userID: 4,
           accepted: true,
           meetingPlace: "Space Mountain",
           dateTime: "2019-01-01 11:00:00",
@@ -42,13 +69,31 @@ exports.seed = function(knex) {
           description: "Most of them are great; one is a trouble maker."
         },
         {
-          id: 5,
-          userID: 2,
+          id: 8,
+          userID: 4,
+          accepted: false,
+          meetingPlace: "Food Court",
+          dateTime: "2019-01-01 20:00:00",
+          kids: 4,
+          description: "My kids are great!"
+        },
+        {
+          id: 9,
+          userID: 5,
           accepted: false,
           meetingPlace: "Cotton Candy Stand",
           dateTime: "2019-01-01 11:00:00",
           kids: 4,
           description: "They can be good. Sometimes..."
+        },
+        {
+          id: 10,
+          userID: 5,
+          accepted: true,
+          meetingPlace: "Food Court",
+          dateTime: "2019-01-01 20:00:00",
+          kids: 4,
+          description: "My kids are great!"
         }
       ]);
     });
