1b8b019264f18e6e19db64f5817facc847fb6cef
devin-bielejec <devin.bieleje@gmail.com>
2019-10-21 17:29:14 -0400

    Finished getby tests

--- users/users-model.spec.js
+++ users/users-model.spec.js
@@ -1,7 +1,7 @@
-const Users = require("../data/dbConfig.js");
-
+const Users = require("./users-model.js");
+const db = require("../data/dbConfig.js");
 function truncateDB() {
-  return Users("users").truncate();
+  return db("users").truncate();
 }
 
 it("should set environment to testing", () => {
@@ -11,19 +11,35 @@ it("should set environment to testing", () => {
 describe("Insert", () => {
   it("DB is empty", async () => {
     await truncateDB();
-    const dbUsers = await Users("users");
+    const dbUsers = await db("users");
     expect(dbUsers.length).toBe(0);
   });
 
   it("DB has a length of 1 after inserting", async () => {
-    await Users("users").insert({
+    const junk = await Users.insert({
       username: "Seth",
       password: "Seth",
       firstName: "Seth",
       lastName: "Seth"
     });
 
-    const dbUsers = await Users("users");
+    console.log(junk);
+
+    const dbUsers = await db("users");
     expect(dbUsers.length).toBe(1);
+  });
+});
+
+describe("getBy", () => {
+  it("DB response has a firstName equal to Seth", async () => {
+    const junk = await Users.insert({
+      username: "Sethy",
+      password: "Seth",
+      firstName: "Seth",
+      lastName: "Seth"
+    });
+
+    const dbRes = await Users.getBy({ firstName: "Seth" });
+    expect(dbRes.firstName).toBe("Seth");
   });
 });
